55bca6fe094ff63b2a47989cc42b46204d464498
Bejie111 <152944068+Bejie111@users.noreply.github.com>
2026-04-23 18:07:24 +0800

    Update students.js

--- db/students.js
+++ db/students.js
@@ -1,104 +1,59 @@
 require('dotenv').config();
 const express = require('express');
-const router = express.Router()
-const hostname = process.env.HOSTNAME
-const username = process.env.USER
-const password = process.env.PASSWORD
-const database = process.env.DATABASE
+const router = express.Router();
 const mysql = require('mysql');
 
-let conn = null;
-const tablename = 'students';
-let fields = [`idno`,`lastname`,`firstname`,`course_id`,`level`];
+const conn = mysql.createPool({
+	host:            process.env.HOSTNAME,
+	user:            process.env.USER,
+	password:        process.env.PASSWORD,
+	database:        process.env.DATABASE,
+	connectionLimit: 10,
+});
 
-function connect(){
-	conn = mysql.createPool({
-		host: hostname,
-		user: username,
-		password: password,
-		database:database
-	});
-}
-
-function disconnect(){
-	if(conn!=null){
-		conn.destroy();
-	}
-}
-
-router.get("/",(req,res)=>{
-	let sql = "SELECT * FROM `"+tablename+"`";
-	connect();
-	conn.query(sql,(err,rows)=>{
-		if(err) return res.status(500).json(err);
+router.get('/', (req, res) => {
+	conn.query('SELECT * FROM students', (err, rows) => {
+		if (err) return res.status(500).json(err);
 		return res.json(rows);
 	});
 });
 
-
-router.get("/:idno",(req,res)=>{
-	let idno = req.params.idno;
-	let sql = "SELECT * FROM `"+tablename+"` WHERE `idno`= ?";
-	connect();
-	conn.query(sql,[idno],(err,rows)=>{
-		if(err) return res.status(500).json(err);
+router.get('/:idno', (req, res) => {
+	conn.query('SELECT * FROM students WHERE idno=?', [req.params.idno], (err, rows) => {
+		if (err) return res.status(500).json(err);
 		return res.json(rows);
 	});
 });
 
-router.delete("/:idno",(req,res)=>{
-	let idno = req.params.idno;
-	let sql = "DELETE FROM `"+tablename+"` WHERE `idno`= ?";
-	connect();
-	conn.query(sql,[idno],(err,rows)=>{
-		if(err) return res.status(500).json(err);
+router.post('/', (req, res) => {
+	const { idno, lastname, firstname, course_id, level } = req.body;
+	conn.query(
+		'INSERT INTO students(idno, lastname, firstname, course_id, level) VALUES(?,?,?,?,?)',
+		[idno, lastname, firstname, course_id, level],
+		(err, rows) => {
+			if (err) return res.status(500).json(err);
+			return res.json(rows);
+		}
+	);
+});
+
+router.put('/:idno', (req, res) => {
+	const { lastname, firstname, course_id, level } = req.body;
+	conn.query(
+		'UPDATE students SET lastname=?, firstname=?, course_id=?, level=? WHERE idno=?',
+		[lastname, firstname, course_id, level, req.params.idno],
+		(err, rows) => {
+			if (err) return res.status(500).json(err);
+			return res.json(rows);
+		}
+	);
+});
+
+router.delete('/:idno', (req, res) => {
+	conn.query('DELETE FROM students WHERE idno=?', [req.params.idno], (err, rows) => {
+		if (err) return res.status(500).json(err);
 		return res.json(rows);
 	});
 });
-
-
-router.post("/",(req,res)=>{
-	let idno = req.body.idno;
-	let lastname = req.body.lastname;
-	let firstname = req.body.firstname;
-	let course_id = req.body.course_id;
-	let level = req.body.level;
-	
-	let sql = "INSERT INTO `"+tablename+"`("+fields+") VALUES(?,?,?,?,?)";
-	connect();
-	conn.query(sql,[idno,lastname,firstname,course_id,level],(err,rows)=>{
-		if(err) return res.status(500).json(err);
-		return res.json(rows);
-	});
-});
-
-
-router.put("/:idno",(req,res)=>{
-	let idno = req.params.idno;
-	let lastname = req.body.lastname;
-	let firstname = req.body.firstname;
-	let course_id = req.body.course_id;
-	let level = req.body.level;
-	
-	let flds = [];
-	for(let i=1;i<fields.length;i++)
-		flds.push(fields[i]+"=?")
-	
-	let fldstr = flds.join(",");
-	
-	let sql = "UPDATE `"+tablename+"` SET "+fldstr+" WHERE "+fields[0]+"="+idno;
-	connect();
-	conn.query(sql,[lastname,firstname,course_id,level],(err,rows)=>{
-		if(err) return res.status(500).json(err);
-		return res.json(rows);
-	});
-});
-
-
-
-
-
 
 module.exports = router;
-
-
